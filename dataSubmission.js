730e16ff0ee74803075d35dfb41ed16bab502aae
Colt Cruz <CBCruz@student.fullsail.edu>
2026-09-24 14:57:46 +0900

    Propagate failed Supabase writes to study flow

--- dataSubmission.js
+++ dataSubmission.js
@@ -71,6 +71,7 @@ async function postToSupabase(table, record, onConflictColumns) {
     }
   } catch (error) {
     console.error(`Could not submit record to Supabase table "${table}" - it is still saved locally.`, error);
+    throw error;
   }
 }
 
